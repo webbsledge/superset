f79c7aca9d03a6efd19a07e969f6a8b31de25de7
Beto Dealmeida <roberto@dealmeida.net>
2026-05-01 23:24:44 -0400

    Add license

--- query-context-sidecar/src/server.ts
+++ query-context-sidecar/src/server.ts
@@ -1,3 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import http from 'http';
 import { URL } from 'url';
 
@@ -103,7 +122,7 @@ async function handleBuildQueryContext(
 
     jsonResponse(res, 200, { query_context: queryContext });
   } catch (err: any) {
-    console.error(`Error building query context for ${formData.viz_type}:`, err);
+    console.error('Error building query context for %s:', formData.viz_type, err);
     jsonResponse(res, 500, {
       error: `Failed to build query context: ${err.message}`,
     });
